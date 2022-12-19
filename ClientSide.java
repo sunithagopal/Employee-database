13b3761b22913ad7e364499ada4b84280626914c
sunithagopal <116723709+sunithagopal@users.noreply.github.com>
2022-12-19 14:51:27 +0530

    socketprogram client

---
+++ ClientSide.java
@@ -0,0 +1,30 @@
+package socketpro;
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClientSide {  
+		public static void main(String[] args) throws Exception{  
+	   
+		Socket s=new Socket("localhost",8888);  
+		DataInputStream din=new DataInputStream(s.getInputStream());
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream()); 
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str=" ",str2=" " ;
+		while(!str.equals("stop")) {
+			str=br.readLine();
+			dout.writeUTF(str);
+		
+		dout.flush();  
+		str2=din.readUTF();
+		System.out.println("Server Says: "+str2);
+		  
+		} 
+		dout.close();
+		s.close();
+		}  
+		}  
